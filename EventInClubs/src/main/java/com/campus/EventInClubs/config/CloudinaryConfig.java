fe00e72074bdc74d199293aaadf77f538d2b1b30
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-11-09 21:44:53 +0530

    cloudinary changes new

--- EventInClubs/src/main/java/com/campus/EventInClubs/config/CloudinaryConfig.java
+++ EventInClubs/src/main/java/com/campus/EventInClubs/config/CloudinaryConfig.java
@@ -2,19 +2,36 @@ package com.campus.EventInClubs.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class CloudinaryConfig {
+    
+    @Value("${cloudinary.cloud-name:dbzrxx75s}")
+    private String cloudName;
+    
+    @Value("${cloudinary.api-key:245416923686528}")
+    private String apiKey;
+    
+    @Value("${cloudinary.api-secret:Kp6R8xFSOtI7dyY4KScCDJCIO5A}")
+    private String apiSecret;
     
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "dbzrxx75s",
-            "api_key", "245416923686528",
-            "api_secret", "Kp6R8xFSOtI7dyY4KScCDJCIO5A",
+        log.info("Initializing Cloudinary with cloud_name: {}", cloudName);
+        
+        Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", cloudName,
+            "api_key", apiKey,
+            "api_secret", apiSecret,
             "secure", true
         ));
+        
+        log.info("Cloudinary initialized successfully");
+        return cloudinary;
     }
 }
